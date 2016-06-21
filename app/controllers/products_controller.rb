c3737a6884fc0e4c53e700b12b185c82927d34a2
kakas <shrimptrain@gmail.com>
2016-06-21 23:12:39 +0800

    done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+
+  def index
+
+  end
+
+  def excel_export
+    @products = Product.all
+    respond_to do |format|
+      format.xls { send_data Product.make_xls(@products), xls_export_params }
+    end
+  end
+
+  private
+
+  def xls_export_params
+    { type: "application/vnd.ms-excel", disposition: 'attachment', filename: "my_first_excel_file.xls" }
+  end
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ActiveRecord::Base
+
+
+  def self.make_xls(products)
+    book = Spreadsheet::Workbook.new
+    sheet1 = book.create_worksheet
+    sheet1.row(0).concat ["產品名稱", "價格"]
+
+    products.each do |product|
+      new_row =  ["#{product.title}", "#{product.price}"]
+      sheet1.insert_row(sheet1.last_row_index + 1, new_row)
+    end
+
+    spreadsheet = StringIO.new
+    book.write spreadsheet
+    spreadsheet.string
+  end
+
 end
